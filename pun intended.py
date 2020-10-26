afb7e0f3e2a049509370b27599a4696454915ca8
ssrosso <sebi.rr@hotmail.com>
2020-10-26 05:29:33 -0300

    Add files via upload

---
+++ pun intended.py
@@ -0,0 +1,14 @@
+class Flower:
+  color = 'unknown'
+
+rose = Flower()
+rose.color = "red"
+
+violet = Flower()
+violet.color = "blue"
+
+pun_intended = "this code has no point, but still here it's you"
+
+print("Roses are {},".format(rose.color))
+print("violets are {},".format(violet.color))
+print(pun_intended) 
